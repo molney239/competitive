e3e9eb44b1d2c5aa67a228a4b703c3584a13e2d8
Timofey <molney239@gmail.com>
2021-11-21 16:54:47 +0300

    Update clion_version.cpp

--- clion_version.cpp
+++ clion_version.cpp
@@ -1,36 +1,23 @@
 #[[#include]]# <iostream>
-#[[#include]]# <vector>
-#[[#include]]# <string>
-#[[#include]]# <algorithm>
-#[[#include]]# <cmath>
-#[[#include]]# <map>
-#[[#include]]# <set>
-#[[#include]]# <queue>
-#[[#include]]# <stack>
 #[[#include]]# <iomanip>
+#[[#include]]# <vector>
+#[[#include]]# <stack>
+#[[#include]]# <queue>
+#[[#include]]# <algorithm>
 using namespace std;
-const bool USE_QUICK_IO = true;
-const bool USE_T = false;
-const unsigned PRECISION = 6;
 
 #[[#define]]# rep(i, n) for (size_t (i) = 0; (i) < (n); (i)++)
 #[[#define]]# repn(i, n) for (size_t (i) = 0; (i) <= (n); (i)++)
 #[[#define]]# rep1(i, n) for (size_t (i) = 1; (i) < (n); (i)++)
 #[[#define]]# rep1n(i, n) for (size_t (i) = 1; (i) <= (n); (i)++)
 #[[#define]]# repr(i, n) for (size_t (i) = (n) - 1; (i) >= 0; (i)--)
-#[[#define]]# pb push_back
 #[[#define]]# all(v) (v).begin(), (v).end()
 #[[#define]]# rall(v) (v).rbegin(), (v).rend()
-#[[#define]]# sze(v) (v).size()
 #[[#define]]# each(x, v) for (auto& (x) : (v))
 #[[#define]]# vec vector
 #[[#define]]# f0r(i, a, b) for (size_t (i) = (a); (i) < (b); (i)++)
 #[[#define]]# f0rn(i, a, b) for (size_t (i) = (a); (i) <= (b); (i)++)
 #[[#define]]# nl "\n"
-#[[#define]]# mp(a, b) make_pair(a, b)
-#[[#define]]# itp(v) make_pair((v).begin(), (v).end())
-#[[#define]]# nw(n, type) type (n); cin >> (n)
-#[[#define]]# nwp(n, type, args...) type(n)(args); cin >> n
 
 #[[#define]]# si short
 #[[#define]]# ll long long
@@ -48,26 +35,15 @@ const unsigned PRECISION = 6;
 #[[#define]]# vpi vector<pair<int, int>>
 #[[#define]]# vvi vector<vi>
 
-template<typename T1, typename T2>
-ostream& operator<<(ostream &os, pair<T1, T2>& p) { return os << p.first << " " << p.second; }
-template<typename T1, typename T2>
-istream& operator>>(istream& in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
-template<typename T>
-ostream& operator<<(ostream &os, vector<T>& v) { each(x, v) os << x << " "; return os; }
-template<typename T>
-istream& operator>>(istream& in, vector<T>& v) { each(x, v) in >> x; return in; }
-template<typename random_it>
-ostream& operator<<(ostream &os, pair<random_it, random_it> it) {
-    while(it.first != it.second) os << *(it.first++) << " ";
-    return os;
-}
-template<typename random_it>
-istream& operator>>(istream& in, pair<random_it, random_it> it) {
-    while(it.first != it.second) in >> it.first++;
-    return in;
-}
-template<typename T>
-bool even(const T& n) { return !(n % 2); }
+template<typename T1, typename T2> ostream& operator<<(ostream &os, pair<T1, T2>& p) { return os << p.first << " " << p.second; }
+template<typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
+template<typename T> ostream& operator<<(ostream &os, vector<T>& v) { each(x, v) os << x << " "; return os; }
+template<typename T> istream& operator>>(istream& in, vector<T>& v) { each(x, v) in >> x; return in; }
+template<typename T> bool even(const T& n) { return !(n % 2); }
+
+#[[#define]]# USE_QUICK_IO
+//#[[#define]]# USE_T
+#[[#define]]# PRECISION 6
 
 
 void solve() {
@@ -79,16 +55,18 @@ void solve() {
 
 int main() {
 
-    if (USE_QUICK_IO) {
+    #[[#ifdef]]# USE_QUICK_IO
         ios_base::sync_with_stdio(false);
         cin.tie(0);
-    }
+    #[[#endif]]#
     cout << setprecision(PRECISION) << fixed;
 
-    if (USE_T) {
+    #[[#ifdef]]# USE_T
         ll t;
         cin >> t;
         rep(i, t) solve();
-    } else solve();
+    #[[#else]]#
+        solve();
+    #[[#endif]]#
 
 }
