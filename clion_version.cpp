363796252edfcd754fefa1bc69824d64f638cad1
Timofey <molney239@gmail.com>
2022-02-10 13:39:45 +0300

    Refactoring
     - Using bits/stdc++ instead of manual including
     - Using pragmas
     - Removing unused macros
     - Using typedef instead of #define
     - Removing I/O for pairs

--- clion_version.cpp
+++ clion_version.cpp
@@ -1,43 +1,26 @@
-#[[#include]]# <iostream>
-#[[#include]]# <iomanip>
-#[[#include]]# <vector>
-#[[#include]]# <stack>
-#[[#include]]# <queue>
-#[[#include]]# <algorithm>
-#[[#include]]# <cmath>
-#[[#include]]# <map>
-#[[#include]]# <set>
-
+#[[#pragma]]# GCC optimize("O3,unroll-loops")
+#[[#pragma]]# GCC diagnostic warning "-Wunused"
+#[[#include]]# <bits/stdc++.h>
 using namespace std;
 
-#[[#define]]# rep(i, n) for (int (i) = 0; (i) < (n); (i)++)
-#[[#define]]# repn(i, n) for (int (i) = 0; (i) <= (n); (i)++)
-#[[#define]]# rep1(i, n) for (int (i) = 1; (i) < (n); (i)++)
-#[[#define]]# rep1n(i, n) for (int (i) = 1; (i) <= (n); (i)++)
+#[[#define]]# rep(i, n) for (int (i) = 0; (i)<(n); (i)++)
+#[[#define]]# rep1(i, n) for (int (i) = 1; (i)<(n); (i)++)
 #[[#define]]# repr(i, n) for (int (i) = (n) - 1; (i) >= 0; (i)--)
 #[[#define]]# all(v) (v).begin(), (v).end()
 #[[#define]]# rall(v) (v).rbegin(), (v).rend()
 #[[#define]]# nl "\n"
 
-#[[#define]]# ll long long
-#[[#define]]# uint unsigned int
-#[[#define]]# ull unsigned long long
-#[[#define]]# ld long double
-#[[#define]]# str string
-#[[#define]]# pi pair<int, int>
-#[[#define]]# pl pair<ll, ll>
+typedef long long ll;
+typedef string str;
+typedef pair<int, int> pi;
+typedef vector<int> vi ;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<pair<int, int>> vpi;
+typedef vector<vi> vvi;
 
-#[[#define]]# vec vector
-#[[#define]]# vi vector<int>
-#[[#define]]# vl vector<ll>
-#[[#define]]# vb vector<bool>
-#[[#define]]# vpi vector<pair<int, int>>
-#[[#define]]# vvi vector<vi>
-
-template<typename T1, typename T2> ostream& operator<<(ostream &os, pair<T1, T2>& p) { return os << p.first << " " << p.second; }
-template<typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
-template<typename T> ostream& operator<<(ostream &os, vector<T>& v) { for (auto x : v) os << x << " "; return os; }
-template<typename T> istream& operator>>(istream& in, vector<T>& v) { for (auto x : v) in >> x; return in; }
+template<typename T> ostream& operator<<(ostream &os, vector<T>& v) { for(auto &x : v) os << x << " "; return os; }
+template<typename T> istream& operator>>(istream &in, vector<T>& v) { for(auto &x : v) in >> x; return in; }
 
 #[[#define]]# USE_QUICK_IO
 //#[[#define]]# USE_T
@@ -52,23 +35,19 @@ void solve() {
 
 
 int main() {
-
     #[[#ifdef]]# USE_FILESTREAM
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #[[#endif]]#
-
     #[[#ifdef]]# USE_QUICK_IO
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
     #[[#endif]]#
     cout << setprecision(6) << fixed;
-
     ll t = 1;
     #[[#ifdef]]# USE_T
         cin >> t;
     #[[#endif]]#
     rep(i, t) solve();
-
 }
