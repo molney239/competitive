c57e7d1c6781e704f7d56c2e3fda6044de66672c
Timofey <molney239@gmail.com>
2022-02-10 13:41:39 +0300

    Create min_clion_version.cpp
    Minimalist version of olymp_template.cpp

---
+++ min_clion_version.cpp
@@ -0,0 +1,34 @@
+#[[#include]]# <bits/stdc++.h>
+using namespace std;
+
+#[[#define]]# rep(i, n) for (int (i) = 0; (i)<(n); (i)++)
+#[[#define]]# all(v) (v).begin(), (v).end()
+#[[#define]]# rall(v) (v).rbegin(), (v).rend()
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi ;
+typedef vector<pair<int, int>> vpi;
+typedef vector<vi> vvi;
+
+
+void solve() {
+    
+    
+    
+}
+
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << setprecision(6) << fixed;
+    
+    ll t = 1;
+    //cin >> t;
+    rep(i, t) solve();
+}
