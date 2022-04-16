172f254b5315a59afb29b5efe57e07f631abde4b
Timofey <molney239@gmail.com>
2022-04-16 19:07:03 +0300

    Overflow protecting, some refactoring

--- lite_clion_version.cpp
+++ lite_clion_version.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 
 #[[#define]]# rep(i, n) for (int (i) = 0; (i)<(n); (i)++)
-#[[#define]]# all(v) (v).begin(), (v).end()
-#[[#define]]# rall(v) (v).rbegin(), (v).rend()
+#[[#define]]# nl "\n"
 
-typedef long long ll;
+#[[#define]]# int32 int32_t
+#[[#define]]# int long long
 typedef pair<int, int> pi;
-typedef vector<int> vi ;
+typedef vector<int> vi;
 typedef vector<pair<int, int>> vpi;
-typedef vector<vi> vvi;
+typedef vector<vector<int>> vvi;
 
 
 void solve() {
-    
-    
-    
+
+
+
 }
 
 
-int main() {
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+int32_t main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cout.tie(nullptr);
     cout << setprecision(6) << fixed;
-    
-    ll t = 1;
+
+    int32_t t = 1;
     //cin >> t;
-    rep(i, t) solve();
+    for (int32_t i = 0; i < t; i++) solve();
 }
