89d614828747273ff9352d299cef094017583629
Timofey <molney239@gmail.com>
2022-04-21 18:34:54 +0300

    Refactoring

--- clion_version.cpp
+++ clion_version.cpp
@@ -1,9 +1,9 @@
 #[[#include]]# <bits/stdc++.h>
 using namespace std;
 
-#[[#define]]# rep(i, n) for (int (i) = 0; (i)<(n); (i)++)
-#[[#define]]# rep1(i, n) for (int (i) = 1; (i)<(n); (i)++)
-#[[#define]]# repr(i, n) for (int (i) = (n) - 1; (i) >= 0; (i)--)
+#[[#define]]# rep(i, n) for (long long (i) = 0; (i) < (n); (i)++)
+#[[#define]]# rep1(i, n) for (long long (i) = 1; (i) < (n); (i)++)
+#[[#define]]# repr(i, n) for (long long (i) = (n) - 1; (i) >= 0; (i)--)
 #[[#define]]# all(v) (v).begin(), (v).end()
 #[[#define]]# rall(v) (v).rbegin(), (v).rend()
 #[[#define]]# nl "\n"
@@ -11,24 +11,24 @@ using namespace std;
 typedef long long ll;
 typedef string str;
 typedef pair<int, int> pi;
-typedef pair<int, int> pl;
+typedef pair<long long, long long> pl;
 typedef vector<int> vi;
-typedef vector<int> vl;
+typedef vector<long long> vl;
 typedef vector<bool> vb;
 typedef vector<pair<int, int>> vpi;
 typedef vector<vector<int>> vvi;
 
-template<typename T> istream& operator>>(istream &in, vector<T>& v) { for (auto &x : v) in >> x; return in; }
-template<typename T> ostream& operator<<(ostream &os, vector<T>& v) { for (auto &x : v) os << x << " "; return os; }
-template<typename T1, typename T2> istream& operator>>(istream &in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
-template<typename T1, typename T2> ostream& operator<<(ostream &os, pair<T1, T2>& p) { os << p.first << " " << p.second; return os; }
+template<typename T> istream& operator>>(istream& in, vector<T>& v) { for (auto& x : v) in >> x; return in; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& x : v) os << x << " "; return os; }
+template<typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
+template<typename T1, typename T2> ostream& operator<<(ostream& os, pair<T1, T2>& p) { os << p.first << " " << p.second; return os; }
 
 //#[[#define]]# USE_QUICK_IO
 //#[[#define]]# USE_T
 //#[[#define]]# USE_FILESTREAM
 
 
-void solve(int test_case) {
+void solve(long long test_case) {
     
     
     
@@ -37,18 +37,18 @@ void solve(int test_case) {
 
 int main() {
     #[[#ifdef]]# USE_FILESTREAM
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
+        std::freopen("input.txt", "r", stdin);
+        std::freopen("output.txt", "w", stdout);
     #[[#endif]]#
     #[[#ifdef]]# USE_QUICK_IO
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
     #[[#endif]]#
-    cout << setprecision(6) << fixed;
+    std::cout << std::setprecision(6) << std::fixed;
     long long t = 1;
     #[[#ifdef]]# USE_T
-        cin >> t;
+        std::cin >> t;
     #[[#endif]]#
     for (long long i = 0; i < t; i++) solve(i + 1);
 }
