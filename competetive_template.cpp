be7056399dcd8a048cb78e4d50674cc03933ea3c
Timofey Hodykin <molney239@gmail.com>
2022-07-27 15:42:54 +0300

    Added min= and max=, references in IO overloading made constant

--- competetive_template.cpp
+++ competetive_template.cpp
@@ -9,8 +9,9 @@ using namespace std;
 //using namespace __gnu_pbds;
 //using namespace __gnu_cxx;
 //typedef tree<int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-const int RANDOM = chrono::duration_cast<chrono::nanoseconds>(chrono::system_clock::now().time_since_epoch()).count();
-struct chash { int operator()(int x) const { return x ^ RANDOM; } };
+
+const int RANDOM_HASH = (int) chrono::steady_clock::now().time_since_epoch().count();
+struct custom_hash { int operator()(int x) const { return x ^ RANDOM_HASH; } };
 
 #define rep(i, n) for (long long (i) = 0; (i) < (long long)(n); ++(i))
 #define rep1(i, n) for (long long (i) = 1; (i) < (long long)(n); ++(i))
@@ -21,20 +22,22 @@ struct chash { int operator()(int x) const { return x ^ RANDOM; } };
 
 typedef long long ll;
 typedef pair<int, int> pi;
-typedef pair<long long, long long> pl;
 typedef vector<int> vi;
 typedef vector<long long> vl;
 typedef vector<bool> vb;
 typedef vector<pair<int, int>> vpi;
 typedef vector<vector<int>> vvi;
 
-istream& operator>>(istream& in, vector<bool>& v) { bool n; for (auto&& i : v) { in >> n; i = n; } return in; }
-template<typename T> istream& operator>>(istream& in, vector<T>& v) { for (auto&& x : v) in >> x; return in; }
-template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto&& x : v) os << x << " "; return os; }
-template<typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
-template<typename T1, typename T2> ostream& operator<<(ostream& os, pair<T1, T2>& p) { os << p.first << " " << p.second; return os; }
+istream& operator>>(istream& in, const vector<bool>& v) { bool n; for (auto&& i : v) { in >> n; i = n; } return in; }
+template<typename T> istream& operator>>(istream& in, const vector<T>& v) { for (auto&& x : v) in >> x; return in; }
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) { for (auto&& x : v) os << x << " "; return os; }
+template<typename T1, typename T2> istream& operator>>(istream& in, const pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
+template<typename T1, typename T2> ostream& operator<<(ostream& os, const pair<T1, T2>& p) { os << p.first << " " << p.second; return os; }
 
-mt19937_64 rnd(chrono::duration_cast<chrono::nanoseconds>(chrono::system_clock::now().time_since_epoch()).count());
+template<typename T1, typename T2> bool chmin(T1& a, const T2& b) { if (a > b) { a = b; return true; } return false; }
+template<typename T1, typename T2> bool chmax(T1& a, const T2& b) { if (a < b) { a = b; return true; } return false; }
+
+mt19937_64 rnd((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
 
 #define QUICK_IO
 //#define MULTIPLE_TESTCASES
@@ -49,7 +52,7 @@ void solve(unsigned test_case) {
 
 
 int32_t main() {
-    #ifndef MOLNEY_LOCAL
+    #ifndef LOCAL
         #ifdef QUICK_IO
             ios_base::sync_with_stdio(false);
             cin.tie(nullptr);
@@ -69,4 +72,4 @@ int32_t main() {
     #endif
     for (unsigned i = 1; i <= t; ++i) solve(i);
     cerr << "execution time = " << clock() - start_time << " ms\n";
-}   
+}
